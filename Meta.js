152673c4aa2f2dc30f0fd9a9b68111f557a46a0f
Brad Robinson <contact@toptensoftware.com>
2025-06-26 14:37:58 +1000

    Icons and colors

--- Meta.js
+++ Meta.js
@@ -47,27 +47,6 @@ export class Meta extends Component
         // Standard
         $.title(c => c.title),
         $.meta.name("description").content(c => c.description),
-
-        // Google/Search
-        $.meta.itemprop("name").content(c => c.name),
-        $.meta.itemprop("description").content(c => c.description),
-        $.meta.itemprop("image").content(c => c.image).if(c => c.image),
-
-        // Facebook
-        $.meta.name("og:url").content(c => c.url),
-        $.meta.name("og:type").content("website"),
-        $.meta.name("og:title").content(c => c.title),
-        $.meta.name("og:description").content(c => c.description),
-        $.meta.name("og:image").content(c => c.image).if(c => c.image),
-
-        // Twitter
-        $.meta.name("twitter:card").content("summary_large_image"),
-        $.meta.name("twitter:title").content(c => c.title),
-        $.meta.name("twitter:description").content(c => c.description),
-        $.meta.name("twitter:image").content(c => c.image).if(c => c.image),
-
-        // Apple
-        $.link.rel("apple-touch-icon").href(c => c.image).if(c => c.image),
         
     ]
 }
