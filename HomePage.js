9fe0a4dc9eb5174cd3f14ca5371b55fd61e26b01
Brad Robinson <brad@toptensoftware.com>
2025-09-01 17:24:39 +1000

    Version

--- HomePage.js
+++ HomePage.js
@@ -96,7 +96,7 @@ export class HomePage extends Component
                                 type: "a",
                                 href: "#",
                                 on_click: () => window.location.reload(),
-                                text: " v0.0.30",
+                                text: " v0.0.31",
                             }
                         ]
                     },
